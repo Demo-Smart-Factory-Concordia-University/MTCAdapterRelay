574eb82fe14c19fd6869bb1cacd28479f31f0b72
Rolf Wuthrich <96795973+rwuthric@users.noreply.github.com>
2022-12-22 10:21:21 -0500

    Added comments

--- demo.py
+++ demo.py
@@ -3,16 +3,23 @@ import sys
 
 
 class DS18B20_MTCAdapterRelayHandler(MTCAdapterRelayHandler):
+    """
+    Serial port connection paramters
+    """
     
     serial_port = '/dev/ttyUSB1'
 
 
 class DS18B20_MTCAdapterRelay(MTCAdapterRelay):
+    """
+    MTConnect adapter serving as a relay to a device connected
+    on the serial port handeld by DS18B20_MTCAdapterRelayHandler
+    """
     
     adapter_port = 8881
     deviceHandler_class = DS18B20_MTCAdapterRelayHandler
 
-    
+
 myAdapter = DS18B20_MTCAdapterRelay()
 try:
     myAdapter.serve_forever()
